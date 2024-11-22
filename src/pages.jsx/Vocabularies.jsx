d48ac6f8d14e306fe5ce1307cd6eba2ce9db51cb
Md Aminul Islam <mr.aminul118@gmail.com>
2024-11-22 14:25:52 +0600

    netlify issue solve

--- src/pages.jsx/Vocabularies.jsx
+++ src/pages.jsx/Vocabularies.jsx
@@ -6,10 +6,15 @@ const Vocabularies = () => {
   console.log(vocabularies);
 
   return (
-    <div className="grid grid-cols-2 md:grid-cols-3 gap-5 md:gap-7 xl:grid-cols-5 xl:gap-12 w-11/12 mx-auto py-14">
-      {vocabularies.map((vocabulary) => (
-        <Vocabulary key={vocabulary.id} vocabulary={vocabulary} />
-      ))}
+    <div>
+      <h1 className="text-3xl text-center py-6 font-semibold mt-12">
+        Total Vocabularies : {vocabularies.length}
+      </h1>
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-5 md:gap-7 xl:grid-cols-5 xl:gap-12 w-11/12 mx-auto py-14">
+        {vocabularies.map((vocabulary) => (
+          <Vocabulary key={vocabulary.id} vocabulary={vocabulary} />
+        ))}
+      </div>
     </div>
   );
 };
